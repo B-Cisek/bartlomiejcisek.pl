b9113c70fe75b9924e6f1405d7595eede5dcc9f7
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2023-12-30 23:20:40 +0100

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down) || true
+    # Install dependencies based on lock file
+    COMPOSER_ALLOW_SUPERUSER=1 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+    # Storage permissions
+    sudo chown -R www-data:www-data storage
+
+    # Migrate database
+    php artisan migrate --force
+
+    # Build assets
+    npm install
+    npm run build
+
+    # Clear cache
+    php artisan clear-compiled
+    php artisan optimize
+
+    # Reload PHP to update opcache
+    sudo -S service php8.3-fpm reload
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
